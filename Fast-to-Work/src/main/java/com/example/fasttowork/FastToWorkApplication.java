bf674ba9e25fc2f82945541de4112dfd55bf6909
SpanDuy <hdhrbux@gmail.com>
2023-09-18 17:13:31 +0300

    feat: add empty project

---
+++ Fast-to-Work/src/main/java/com/example/fasttowork/FastToWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.fasttowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastToWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastToWorkApplication.class, args);
+	}
+
+}
---
+++ Fast-to-Work/src/test/java/com/example/fasttowork/FastToWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fasttowork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastToWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
